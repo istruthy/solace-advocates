e79b6a01683593fc8f3502262f012ad977eeccc3
istruthy <daren.lunsford+istruthy@gmail.com>
2025-08-09 01:16:23 -0500

    task: denotes that needs to run on localhost:3000

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiUrl: 'http://localhost:3000';
+  apiUrl: 'http://localhost:3000', // this needs to be made dynamic
   databaseUrl: process.env.DATABASE_URL,
   
   validate: () => {
